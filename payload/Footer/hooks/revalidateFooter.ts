c12fb6f4be4e72133ae4f82be31593fca6344283
unknown <rafaelcon96@gmail.com>
2025-11-20 13:34:30 +0200

    ready to build2

--- payload/Footer/hooks/revalidateFooter.ts
+++
@@ -1,9 +0,0 @@
-import type { GlobalAfterChangeHook } from 'payload'
-
-export const revalidateFooter: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
-  if (!context.disableRevalidate) {
-    payload.logger.info(`Revalidating footer`)
-  }
-
-  return doc
-}
--- payload/Header/hooks/revalidateHeader.ts
+++
@@ -1,9 +0,0 @@
-import type { GlobalAfterChangeHook } from 'payload'
-
-export const revalidateHeader: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
-  if (!context.disableRevalidate) {
-    payload.logger.info(`Revalidating header`)
-  }
-
-  return doc
-}
